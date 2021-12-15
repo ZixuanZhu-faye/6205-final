fe374fbdb42d569f99e65d9e00d5033805e0d364
ZixuanZhu-faye <zhu.zixu@outlook.com>
2021-12-15 15:18:41 -0500

    final

---
+++ src/Pell.java
@@ -0,0 +1,27 @@
+public class Pell {
+    public Pell() {
+    }
+
+    public long get(int n) {
+	if (n < 0)
+	    throw new UnsupportedOperationException("Pell.get is not supported for negative n");
+
+	if (n == 0) {
+	    return 0;
+	}
+
+	if (n == 1) {
+	    return 1;
+	}
+
+	long[] arr = new long[n + 1];
+	arr[0] = 0;
+	arr[1] = 1;
+
+	for (int i = 2; i <= n; i++) {
+	    arr[i] = 2 * arr[i - 1] + arr[i - 2];
+	}
+
+	return arr[n];
+    }
+}
